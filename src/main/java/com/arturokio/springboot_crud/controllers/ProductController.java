49bc0d7c205fc9da653953621e6bbc7bcaf92793
acr00058 <acr00058@red.ujaen.es>
2025-11-17 16:10:45 +0100

    183 productcontroller

---
+++ src/main/java/com/arturokio/springboot_crud/controllers/ProductController.java
@@ -0,0 +1,52 @@
+package com.arturokio.springboot_crud.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arturokio.springboot_crud.entities.Product;
+import com.arturokio.springboot_crud.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> list() {
+        return service.findAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> view(@PathVariable Long id) {
+        Optional<Product> product = service.findById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product.orElseThrow());
+        } else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        service.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        Optional<Product> prod=service.findById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.save(product));
+    }
+
+}
